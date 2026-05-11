3b6c6b8efa902d2573670265d0f8035f65722f67
lakshya747 <pandeylakshya51s@gmail.com>
2026-05-11 13:36:12 +0530

    Implement typing effect and fix email form submission
    Added typing effect for text display and improved email form handling with EmailJS.

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+// typing effect
+const text = ["Frontend Developer", "Web Designer"];
+let i=0, j=0, current="", del=false;
+
+function type(){
+    let el=document.getElementById("typing");
+
+    if(i<text.length){
+        if(!del && j<=text[i].length){
+            current=text[i].substring(0,j++);
+        }else if(del && j>=0){
+            current=text[i].substring(0,j--);
+        }
+
+        el.innerHTML=current;
+
+        if(j===text[i].length) del=true;
+        if(j===0){del=false;i++;}
+
+        setTimeout(type, del?100:200);
+    }else{i=0;type();}
+}
+type();
+
+
+// EMAILJS FIX
 document.addEventListener("DOMContentLoaded", function() {
 
-  document.getElementById("contact-form")
-  .addEventListener("submit", function(e) {
+  emailjs.init("62KuctRvN9v5M3DxM");
+
+  const form = document.getElementById("contact-form");
+
+  if(!form){
+    alert("Form not found ❌");
+    return;
+  }
+
+  form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     emailjs.sendForm(
@@ -18,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .then(() => {
       alert("Message Sent Successfully 😎");
+      form.reset();
     })
     .catch((error) => {
       alert("Failed 😢 " + JSON.stringify(error));
