59045e3be5f3e4548367887ac57f9567f392752f
lakshya747 <pandeylakshya51s@gmail.com>
2026-05-11 20:46:28 +0530

    Refactor typing effect and update email service ID

--- script.js
+++ script.js
@@ -39,13 +39,13 @@ document.addEventListener("DOMContentLoaded", function () {
         e.preventDefault();
 
         emailjs.sendForm(
-            "service_iql6qas",
+            "service_iql7qas",
             "template_rt16qr6", 
             this
         )
         .then(() => {
             return emailjs.sendForm(
-                " template_rt16qr6",
+                "template_rt16qr6",
                 "template_reply",
                 this
             );
